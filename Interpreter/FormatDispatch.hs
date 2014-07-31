502cd2b898d68d4691dfe39c1255c87627348548
Anton Golov <jesyspa@gmail.com>
2014-08-01 00:39:35 +0200

    Improved dynamic dispatch.

--- Interpreter/FormatDispatch.hs
+++ Interpreter/FormatDispatch.hs
@@ -18,29 +18,29 @@ data MStack a
 
 data family Lambda :: * -> (* -> *) -> * -> *
 
-data instance Lambda (Hole -> MStack ()) m a = Lambda1ToM { getLambda1ToM :: a -> m () }
-data instance Lambda (Hole -> Hole -> MStack ()) m a = Lambda2ToM { getLambda2ToM :: a -> a -> m () }
-data instance Lambda (Hole -> Hole -> Hole) m a = Lambda2ToA { getLambda2ToA :: a -> a -> a }
+data instance Lambda (Hole -> r) m a = LambdaTakingHole (a -> Lambda r m a)
+data instance Lambda (MStack ()) m a = LambdaMStack { getLambdaMStack :: m () }
+data instance Lambda Hole m a = LambdaHole { getLambdaHole :: a }
 
 class LambdaLike fn where
     type family UnderlyingFun fn (m :: * -> *) a
     wrapf :: UnderlyingFun fn m a -> Lambda fn m a
     unwrapf :: Lambda fn m a -> UnderlyingFun fn m a
 
-instance LambdaLike (Hole -> MStack ()) where
-    type UnderlyingFun (Hole -> MStack ()) m a = a -> m ()
-    wrapf = Lambda1ToM
-    unwrapf = getLambda1ToM
+instance LambdaLike r => LambdaLike (Hole -> r) where
+    type UnderlyingFun (Hole -> r) m a = a -> UnderlyingFun r m a
+    wrapf f = LambdaTakingHole (\x -> wrapf (f x))
+    unwrapf (LambdaTakingHole f) = unwrapf . f
 
-instance LambdaLike (Hole -> Hole -> MStack ()) where
-    type UnderlyingFun (Hole -> Hole -> MStack ()) m a = a -> a -> m ()
-    wrapf = Lambda2ToM
-    unwrapf = getLambda2ToM
+instance LambdaLike (MStack ()) where
+    type UnderlyingFun (MStack ()) m a = m ()
+    wrapf = LambdaMStack
+    unwrapf = getLambdaMStack
 
-instance LambdaLike (Hole -> Hole -> Hole) where
-    type UnderlyingFun (Hole -> Hole -> Hole) m a = a -> a -> a
-    wrapf = Lambda2ToA
-    unwrapf = getLambda2ToA
+instance LambdaLike Hole where
+    type UnderlyingFun Hole m a = a
+    wrapf = LambdaHole
+    unwrapf = getLambdaHole
 
 dispatch :: MonadStack m => (forall a. NumericRep a => Lambda fn m a -> Proxy a -> m ()) -> (forall a. NumericRep a => Lambda fn m a) -> Format -> m ()
 dispatch fn f = go
